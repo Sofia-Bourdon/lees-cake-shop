c1ac26f3d7fa67267e0ec050c615593793c3e623
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-02-08 11:35:24 +0000

    Create add_sales_data function to get input data from user

--- run.py
+++ run.py
@@ -12,8 +12,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('lees_cake_shop')
 
-sales = SHEET.worksheet('sales')
+sales = SHEET.worksheet('favorites')
 
 data = sales.get_all_values()
 
-print(data)
+
+def add_sales_data():
+    """
+    Updates the values given by the user to the spreadsheet 'sales'
+    """
+    print("Please enter your weekly sales data.")
+    print("Only 10 values from 1 to 20 are accepted and the data must be separated by commas.")
+    print("Example: 2,4,6,8,10,12,14,16,18,20")
+    data_str = input("Insert data: ")
+    print(f"You inserted {data_str}")
+
+
+add_sales_data()
