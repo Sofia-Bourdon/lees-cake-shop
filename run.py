9efb9c3a34f8ad236e56ed4ef6c9042817372229
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-02-08 13:47:04 +0000

    Create function to convert the data given into Integers and check for exatcly 10 values provided

--- run.py
+++ run.py
@@ -23,9 +23,25 @@ def add_sales_data():
     """
     print("Please enter your weekly sales data.")
     print("Only 10 values from 1 to 20 are accepted and the data must be separated by commas.")
-    print("Example: 2,4,6,8,10,12,14,16,18,20")
-    data_str = input("Insert data: ")
-    print(f"You inserted {data_str}")
+    print("Example: 2,4,6,8,10,12,14,16,18,20.")
+
+    data_input = input("Insert data: ").split(",")
+    validate_number(data_input)
+
+
+
+def validate_number(values):
+    """
+    Converts the data given by user into Integers.
+    Checks for the right number of values (10) If not, Raises ValueError.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 10:
+            raise ValueError()
+    except ValueError:
+        print(f"10 numbers needed to complete this operation. You provided {len(values)}. Please try again. \n")
+
 
 
 add_sales_data()
